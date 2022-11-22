5019d881e9a0c91551d179790e6816edf78a243a
Adilson Jr <adylsonjr@hotmail.com>
2022-11-22 13:06:36 -0300

    Feature login page and modal forget password

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,13 @@
-/* Desenvolva seu código aqui... */
+const controlModal = document.querySelector("span")
+
+controlModal.addEventListener("click", ()=>{
+        controlModal.getAttribute("data-control-modal")
+        document.getElementById("modal-login").classList.toggle("show-modal")
+    })
+
+const closeModal = document.querySelector(".modal-close")
+
+closeModal.addEventListener("click", ()=>{
+    closeModal.getAttribute("data-control-modal")
+    document.getElementById("modal-login").classList.toggle("show-modal")
+})
